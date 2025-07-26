6f42b7470d4eeb20827a9fd7a762c007d416c4fc
Alek-Grujic <alek.grujic001@outlook.com>
2025-07-26 07:03:58 +0200

    Print forecasted temperatures using forEach with day index

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('testing prettier');
+const temp = [17, 21, 23];
+
+function printForecast(arr) {
+  //   let days = 1;
+  //   for (let key of arr) {
+  //     console.log(`${key}C in ${days++} days`);
+  //   }
+  const array = arr.forEach((arr, index) => {
+    console.log(`${arr}C in ${index + 1} days`);
+  });
+}
+
+printForecast(temp);
+// console.log(printForecast(temp));
