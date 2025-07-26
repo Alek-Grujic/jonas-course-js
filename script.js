588fa7818ff2e2eca2aea57676337b0694d8a153
Alek-Grujic <alek.grujic001@outlook.com>
2025-07-26 07:11:35 +0200

    Format temperature forecast string using for loop and log output

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+// const temp = [17, 21, 23];
+
+// function printForecast(arr) {
+//   //   let days = 1;
+//   //   for (let key of arr) {
+//   //     console.log(`${key}C in ${days++} days`);
+//   //   }
+//   const array = arr.forEach((arr, index) => {
+//     console.log(`${arr}C in ${index + 1} days`);
+//   });
+// }
+
+// printForecast(temp);
+
+// ------------------------------
+
 const temp = [17, 21, 23];
 
 function printForecast(arr) {
-  //   let days = 1;
-  //   for (let key of arr) {
-  //     console.log(`${key}C in ${days++} days`);
-  //   }
-  const array = arr.forEach((arr, index) => {
-    console.log(`${arr}C in ${index + 1} days`);
-  });
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}C in ${i + 1} days ...`;
+  }
+  console.log('...', str);
 }
 
 printForecast(temp);
-// console.log(printForecast(temp));
+
+// ------------------------------
