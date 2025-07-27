0248adfb2b81bcd5f808a9c5f1bb8f97a11fd343
Alek-Grujic <alek.grujic001@outlook.com>
2025-07-27 16:22:54 +0200

    Start Time tracking app for freelancers

--- script.js
+++ script.js
@@ -14,16 +14,41 @@
 
 // ------------------------------
 
-const temp = [17, 21, 23];
+// const temp = [17, 21, 23];
 
-function printForecast(arr) {
-  let str = '';
-  for (let i = 0; i < arr.length; i++) {
-    str += `${arr[i]}C in ${i + 1} days ...`;
-  }
-  console.log('...', str);
-}
+// function printForecast(arr) {
+//   let str = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     str += `${arr[i]}C in ${i + 1} days ...`;
+//   }
+//   console.log('...', str);
+// }
 
-printForecast(temp);
+// printForecast(temp);
 
 // ------------------------------
+
+// Time tracking app for freelancers
+
+function AnalyzeWorkWeek() {
+  this.totalHours = function (array) {
+    const sum = array.reduce((acc, curr) => acc + curr);
+    if (array.length > 7) {
+      throw new Error('Array has more than 7 digits!');
+    }
+    return sum;
+  };
+  this.averageDailyHours = function (array) {
+    if (array.length > 7) {
+      throw new Error('Array has more than 7 digits!');
+    }
+    const average = array.reduce((acc, curr) => acc + curr);
+    const twoDecimels = average / 7;
+    return twoDecimels.toFixed(2);
+  };
+}
+const markDate = [7.5, 8, 6.5, 0, 8.5, 4, 0];
+const mark = new AnalyzeWorkWeek();
+
+console.log(mark.totalHours(markDate));
+console.log(mark.averageDailyHours(markDate));
