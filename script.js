acaa187631a72f8e58bdf773b8024c9dfb800af9
Alek-Grujic <alek.grujic001@outlook.com>
2025-07-29 14:17:11 +0200

    Analyze work week and convert hours to hours:minutes format

--- script.js
+++ script.js
@@ -30,25 +30,85 @@
 
 // Time tracking app for freelancers
 
-function AnalyzeWorkWeek() {
-  this.totalHours = function (array) {
-    const sum = array.reduce((acc, curr) => acc + curr);
-    if (array.length > 7) {
-      throw new Error('Array has more than 7 digits!');
-    }
-    return sum;
-  };
-  this.averageDailyHours = function (array) {
-    if (array.length > 7) {
-      throw new Error('Array has more than 7 digits!');
-    }
-    const average = array.reduce((acc, curr) => acc + curr);
-    const twoDecimels = average / 7;
-    return twoDecimels.toFixed(2);
-  };
-}
-const markDate = [7.5, 8, 6.5, 0, 8.5, 4, 0];
-const mark = new AnalyzeWorkWeek();
+// Koja je najbolja metoda da se ovo uradi?
+//
 
-console.log(mark.totalHours(markDate));
-console.log(mark.averageDailyHours(markDate));
+// function AnalyzeWorkWeek() {
+//   this.totalHours = function (array) {
+//     const sum = array.reduce((acc, curr) => acc + curr);
+//     if (array.length > 7) {
+//       throw new Error('Array has more than 7 digits!');
+//     }
+//     return sum;
+//   };
+//   this.averageDailyHours = function (array) {
+//     if (array.length > 7) {
+//       throw new Error('Array has more than 7 digits!');
+//     }
+//     const average = array.reduce((acc, curr) => acc + curr);
+//     const twoDecimels = average / 7;
+//     return twoDecimels.toFixed(2);
+//   };
+// }
+// const markDate = [7.5, 8, 6.5, 0, 8.5, 4, 0];
+// const mark = new AnalyzeWorkWeek();
+
+// console.log(mark.totalHours(markDate));
+// console.log(mark.averageDailyHours(markDate));
+// console.log(mark);
+
+// function AnalyzeWorkWeek(array) {
+//   let days = [
+//     'monday',
+//     'tuesday',
+//     'wednesday',
+//     'thursday',
+//     'friday',
+//     'saturday',
+//     'sunday',
+//   ];
+//   this.arr = array;
+//   this.totalWorkHours = this.arr.reduce((acc, curr) => acc + curr);
+//   this.averageDailyHours = parseFloat((this.totalWorkHours / 7).toFixed(2));
+//   this.dayMostHours = this.arr.reduce((acc, curr) => (acc > curr ? acc : curr));
+//   Object.defineProperty(this, 'info', {
+//     get: function () {
+//       if (array.length > 7) {
+//         throw new Error('Array has more than 7 digits!');
+//       }
+//       console.log(`Total work hours: ${this.totalWorkHours}
+// Average daily hours: ${this.averageDailyHours}`);
+//     },
+//   });
+// }
+// const mark = new AnalyzeWorkWeek([7.5, 8, 6.5, 0, 8.5, 4, 0]);
+
+// console.log(mark);
+// mark.info;
+
+// ---------- functional programming
+
+const array = [7.5, 8, 6.5, 0, 8.5, 4, 0];
+
+// function totalWorkWeek(total) {
+//   return total.reduce((acc, curr) => acc + curr);
+// }
+
+function analyzeWorkWeek(arr) {
+  // total work hours
+  const totalWorkHours = arr.reduce((acc, curr) => acc + curr);
+  console.log(`Total hours worked: ${minutes(totalWorkHours)}`);
+
+  // average daily hours
+  const averageDailyHours = (totalWorkHours / 7).toFixed(2);
+  console.log(`Average daily hours: ${minutes(averageDailyHours)}`);
+}
+
+// function that turns numbers into hours and minutes
+function minutes(number) {
+  const fullHours = Math.floor(Number(number));
+  const remainingMinutes = Math.round((number - fullHours) * 60);
+  return `${fullHours}h ${remainingMinutes}min`;
+}
+
+analyzeWorkWeek(array);
